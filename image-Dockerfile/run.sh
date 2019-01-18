64fd958013017eb4bf3159f5a9b5691084b3661b
siangyeh8818 <siangyeh8818@gmail.com>
2019-01-18 18:04:09 +0800

    setting storage.tsdb.retention to 7d

--- image-Dockerfile/run.sh
+++ image-Dockerfile/run.sh
@@ -2,4 +2,4 @@
 
 chmod 777 -R /opt/prometheus
 
-/prometheus/prometheus --config.file="/prometheus/prometheus.yml" --storage.tsdb.path=/opt/prometheus 
+/prometheus/prometheus --config.file="/prometheus/prometheus.yml" --storage.tsdb.path=/opt/prometheus --storage.tsdb.retention=7d 
